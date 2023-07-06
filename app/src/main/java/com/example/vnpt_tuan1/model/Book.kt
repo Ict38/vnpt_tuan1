bfce0882d1330339d28319f5c544969fe19ac11e
HC <HUYCD.B19CN304@stu.ptit.edu.vn>
2023-07-06 23:18:25 +0700

    init

---
+++ app/src/main/java/com/example/vnpt_tuan1/model/Book.kt
@@ -0,0 +1,11 @@
+package com.example.vnpt_tuan1.model
+
+data class Book(
+    val id: Int,
+    val name : String,
+    val author : String,
+    val releaseDate : String,
+    val publisher : String,
+    val price : Float
+) {
+}
