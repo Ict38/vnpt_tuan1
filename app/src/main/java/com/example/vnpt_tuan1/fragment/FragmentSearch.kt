e881f8d3a92eda9bba1a73fc2b2351b0fc55b7e1
hc <chuhuy3821@gmail.com>
2023-07-07 14:34:31 +0700

    search

--- app/src/main/java/com/example/vnpt_tuan1/fragment/FragmentSearch.kt
+++ app/src/main/java/com/example/vnpt_tuan1/fragment/FragmentSearch.kt
@@ -1,60 +1,101 @@
 package com.example.vnpt_tuan1.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vnpt_tuan1.R
+import com.example.vnpt_tuan1.activity.UpdateDeleteActivity
+import com.example.vnpt_tuan1.adapter.RecyclerViewAdapter
+import com.example.vnpt_tuan1.data.AppDatabase
+import com.example.vnpt_tuan1.data.BookDAO
+import com.example.vnpt_tuan1.databinding.FragmentSearchBinding
+import com.example.vnpt_tuan1.model.Book
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentSearch.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FragmentSearch : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class FragmentSearch : Fragment(), RecyclerViewAdapter.OnItemClickListener {
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapter: RecyclerViewAdapter
+    private lateinit var bookDAO: BookDAO
+    private lateinit var bookList : ArrayList<Book>
+    private lateinit var tempBookList : ArrayList<Book>
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentSearch.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentSearch().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val db = context?.let { AppDatabase.getInstance(it) }
+        bookDAO = db?.BookDAO()!!
+        tempBookList = bookDAO.getAll() as ArrayList<Book>
+        bookList = bookDAO.getAll() as ArrayList<Book>
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                tempBookList.clear()
+                val searchText = query!!.lowercase(Locale.getDefault())
+                if(searchText.isNotEmpty()){
+                    bookList.forEach {
+                        if(it.name?.lowercase(Locale.getDefault())?.contains(searchText) == true){
+                            tempBookList.add(it)
+                        }
+                    }
+                    tempBookList.forEach {
+                        Log.e("book temp", it.toString())
+                    }
+                    adapter.notifyDataSetChanged()
                 }
+                else{
+                    tempBookList.addAll(bookList)
+                    adapter.notifyDataSetChanged()
+                }
+                return false
             }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                tempBookList.clear()
+                Log.e("BOOK1", tempBookList.toString())
+                Log.e("BOOK2", bookList.toString())
+                val searchText = newText!!.lowercase(Locale.getDefault())
+                if(searchText.isNotEmpty()){
+                    bookList.forEach {
+                        if(it.name?.lowercase(Locale.getDefault())?.contains(searchText) == true){
+                            tempBookList.add(it)
+                        }
+                    }
+                    tempBookList.forEach {
+                        Log.e("book temp", it.toString())
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                else{
+                    tempBookList.addAll(bookList)
+                    adapter.notifyDataSetChanged()
+                }
+                return false
+            }
+        })
+        adapter = RecyclerViewAdapter(tempBookList, this)
+        adapter.setBookList(tempBookList)
+        val manager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        binding.rview.layoutManager = manager
+        binding.rview.adapter = adapter
+    }
+
+    override fun onItemClick(position: Int) {
+        val book: Book = adapter.getItem(position)
+        val intent = Intent(activity, UpdateDeleteActivity::class.java)
+        intent.putExtra("book", book)
+        startActivity(intent)
     }
 }
